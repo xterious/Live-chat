0d3671daa660756eac5841e4640b318b21c3c361
Kamalesh <kamalesh2023@gmail.com>
2023-11-26 12:24:04 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import Peer from "peerjs";
+import "./App.css";
 
 function App() {
+  const [peerid, setPeerid] = useState(null);
+  const [remotePeerIdValue, setRemotePeerIdValue] = useState("");
+  const remoteVideoRef = useRef(null);
+  const currentVideoRef = useRef(null);
+
+  const peerReference = useRef(null);
+
+  useEffect(() => {
+    const peer = new Peer();
+
+    peer.on("open", (id) => {
+      setPeerid(id);
+    });
+    peer.on("call", (call) => {
+      var getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia;
+
+      getUserMedia({ video: true, audio: true }, (mediaStream) => {
+        currentVideoRef.current.srcObject = mediaStream;
+        currentVideoRef.current.play();
+        call.answer(mediaStream);
+        call.on("stream", (remoteStream) => {
+          remoteVideoRef.current.srcObject = remoteStream;
+          remoteVideoRef.current.play();
+        });
+      });
+    });
+
+    peerReference.current = peer;
+  }, []);
+
+  const call = (remotePeerId) => {
+    var getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+    getUserMedia({ video: true, audio: true }, (mediaStream) => {
+      currentVideoRef.current.srcObject = mediaStream;
+      currentVideoRef.current.play();
+
+      const call = peerReference.current.call(remotePeerId, mediaStream);
+
+      call.on("stream", (remoteStream) => {
+        // Show stream in some video/canvas element.
+        remoteVideoRef.current.srcObject = remoteStream;
+        remoteVideoRef.current.play();
+      });
+    });
+  };
+
+  console.log(peerid);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Current user id : <span>{peerid}</span>
+      </h1>
+      <input
+        type="text"
+        value={remotePeerIdValue}
+        onChange={(e) => setRemotePeerIdValue(e.target.value)}
+      />
+      <button onClick={() => call(remotePeerIdValue)}>Call</button>
+      <div>
+        <video ref={currentVideoRef} />
+      </div>
+      <div>
+        <video ref={remoteVideoRef} />
+      </div>
     </div>
   );
 }
